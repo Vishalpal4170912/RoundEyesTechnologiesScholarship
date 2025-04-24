d35770a7ca70d6373a78c6d143381f5b8afc66de
Vishalpal4170912 <167093332+Vishalpal4170912@users.noreply.github.com>
2025-04-24 12:40:41 +0530

    Added all these

---
+++ CSR.js
@@ -0,0 +1,6 @@
+function getStarted() {
+    alert("Thank you for getting started! Our team will reach out to you soon.");
+    // You can also redirect to a contact page:
+    // window.location.href = "contact.html";
+  }
+  
