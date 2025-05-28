8b8af7619339215b250715e73f4a2da4b77a4abc
Vishalpal4170912 <167093332+Vishalpal4170912@users.noreply.github.com>
2025-05-28 16:08:51 +0530

    Update CSR.js

--- CSR.js
+++ CSR.js
@@ -3,4 +3,20 @@ function getStarted() {
     // You can also redirect to a contact page:
     // window.location.href = "contact.html";
   }
+<!-- Live Agent Script -->
+<script type='text/javascript' src='https://c.la1-c2cs-phx.salesforceliveagent.com/content/g/js/58.0/deployment.js'></script>
+<script type='text/javascript'>
+  liveagent.init('https://d.la1-c2cs-phx.salesforceliveagent.com/chat', 'YOUR_DEPLOYMENT_ID', 'YOUR_ORG_ID');
+
+  if (!window._laq) { window._laq = []; }
+  window._laq.push(function () {
+    liveagent.showWhenOnline('YOUR_BUTTON_ID', document.getElementById('liveagent_button_online'));
+    liveagent.showWhenOffline('YOUR_BUTTON_ID', document.getElementById('liveagent_button_offline'));
+  });
+
+  function startLiveChat() {
+    liveagent.startChat('YOUR_BUTTON_ID');
+  }
+</script>
+
   
